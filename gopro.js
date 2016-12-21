1b12eb4e0630b1eb2138e31b11d93c76da52d942
Matthew Twomey <mtwomey@beakstar.com>
2016-12-20 21:35:48 -0600

    Added error handeling to gopro takepicture

--- gopro.js
+++ gopro.js
@@ -29,7 +29,11 @@ function readGPIO(gpio){
 
 function takePicture(){
     request.get('http://10.5.5.9/gp/gpControl/command/mode?p=1', (() => {
-        request.get('http://10.5.5.9/gp/gpControl/command/shutter?p=1');
+        request.get('http://10.5.5.9/gp/gpControl/command/shutter?p=1').on('error', (err => {
+            console.log(err);
+        }));
+    })).on('error', (err => {
+        console.log(err);
     }));
 }
 
